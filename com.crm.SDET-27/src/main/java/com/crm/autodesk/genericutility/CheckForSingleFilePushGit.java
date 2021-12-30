b1a2bf75526bee155fb04d17eba237ff6bfb5a31
AJAY KUMAR S <AJAY KUMAR S@DESKTOP-53K956R>
2021-12-30 10:39:31 +0530

    single file check

---
+++ com.crm.SDET-27/src/main/java/com/crm/autodesk/genericutility/CheckForSingleFilePushGit.java
@@ -0,0 +1,10 @@
+package com.crm.autodesk.genericutility;
+
+import org.testng.annotations.Test;
+
+public class CheckForSingleFilePushGit {
+	@Test
+	public  void executeSingle() {
+		System.out.println("hello");
+	}
+}
